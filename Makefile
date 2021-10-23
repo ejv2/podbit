d4b223d72f44aff67f52a381f1b5ea856333face
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-10-23 23:02:06 +0100

    Begin adding table component
    For use in the queue and download menus

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 EXE = podbit
 
 UISRC    = ui/ui.go ui/input.go colors/colors.go ui/library.go ui/player.go ui/queue.go ui/tray.go
-UICOMPS  = ui/components/menu.go
+UICOMPS  = ui/components/menu.go ui/components/table.go
 SOUNDSRC = sound/sound.go sound/queue.go
 DATASRC  = data/data.go data/queue.go data/db.go data/cache.go
 SRC = main.go ver.go ${INPUTSRC} ${UISRC} ${DATASRC} ${UICOMPS} ${SOUNDSRC}
---
+++ ui/components/table.go
@@ -0,0 +1,102 @@
+package components
+
+import (
+	"github.com/rthornton128/goncurses"
+)
+
+// A Column is a vertical section of a table, defined by a
+// pair of both a "width" and a "label".
+//
+// The width is the fraction of the table width the column
+// should take up (defined between zero and one). Table's
+// render method will panic if any column has a "width"
+// of less than zero or greater than one, or if an impossible
+// combination of widths was requested.
+//
+// The width is the label which will be displayed at the top of the table
+// The color is the ncurses color to activate when selected
+type Column struct {
+	Label string
+	Width float64
+	Color int16
+}
+
+// Table represents a vertical, headed table structure, useful
+// for displaying a slice of structs
+type Table struct {
+	X, Y int
+	W, H int
+	Win  *goncurses.Window
+
+	// See column struct for docs
+	Columns []Column
+	// Each sub-slice represents each column entry (eg, 0 = first column)
+	Items [][]string
+
+	scroll int
+	sel    int
+
+	prevw, prevh int
+}
+
+func (t *Table) Render() {
+	items := t.Items[t.scroll:]
+	if t.prevw != t.W || t.prevh != t.H {
+		t.scroll = 0
+	}
+	t.prevw, t.prevh = t.W, t.H
+
+	t.Win.AttrOn(goncurses.A_BOLD)
+	t.Win.HLine(t.Y+1, t.X, goncurses.ACS_HLINE, t.W)
+	t.Win.AttrOff(goncurses.A_BOLD)
+
+	off := 0
+	for i, elem := range t.Columns {
+		colw := int(float64(t.W) * elem.Width)
+		if off > t.W || colw > t.W {
+			panic("invalid table header: impossible column width")
+		}
+
+		if len(elem.Label) > colw {
+			elem.Label = elem.Label[:colw]
+		}
+
+		t.Win.AttrOn(goncurses.A_BOLD)
+		t.Win.MovePrint(t.Y, t.X+off, elem.Label)
+		t.Win.AttrOff(goncurses.A_BOLD)
+
+		c := t.scroll
+		for j, entry := range items {
+			if i >= len(entry) {
+				panic("invalid table entry: missing fields")
+			}
+
+			capped := entry[i]
+			sel := c == t.sel
+
+			if len(capped) > colw {
+				// Trim to fit row
+				capped = capped[:colw]
+			} else {
+				// Pad out to fill row
+				for i := len(capped); i <= colw; i++ {
+					capped += " "
+				}
+			}
+
+			if sel {
+				t.Win.ColorOn(elem.Color)
+			}
+
+			t.Win.MovePrint(t.Y+j+2, t.X+off, capped)
+
+			if sel {
+				t.Win.ColorOff(elem.Color)
+			}
+
+			c++
+		}
+
+		off += colw
+	}
+}
